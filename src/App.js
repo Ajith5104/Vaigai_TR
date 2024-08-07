33eab7a245efc017f930ae823ca7391f82c2d82a
Ajith5104 <ajithkumar51046@gmail.com>
2024-08-07 22:09:59 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import "./App.css";
+import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Service from "./Components/Service";
